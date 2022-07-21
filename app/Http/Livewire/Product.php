c0d55da3251064066d8c0692949a01ab1b71762e
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-07-21 09:06:40 +0700

    upload image & preview

--- app/Http/Livewire/Product.php
+++ app/Http/Livewire/Product.php
@@ -3,15 +3,27 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use App\Models\Product as ProducModel;
 
 class Product extends Component
 {
+    use WithFileUploads;
+
+    public $name, $image, $description, $qty, $price;
+
     public function render()
     {
         $products = ProducModel::orderBy('created_at', 'DESC')->get();
         return view('livewire.product', [
             'products' => $products
+        ]);
+    }
+
+    public function previewImage()
+    {
+        $this->validate([
+            'image' => 'image|max:2048'
         ]);
     }
 }
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -35,6 +35,33 @@
             <div class="card">
                 <div class="card-body">
                     <h2 class="fw-bold mb-3">Create Product</h2>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Product Name</label>
+                            <input wire:model="name" type="email" class="form-control" id="exampleFormControlInput1"
+                                placeholder="">
+                            @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <label>Product Image</label>
+                        <div class="input-group mb-3">
+                            <input wire:model="image" type="file" class="form-control" id="inputGroupFile02">
+                            <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                            @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        @if ($image)
+                            <label class="mt-2">Image Preview:</label>
+                            <img src="{{ $image->temporaryUrl() }}" class="img-fluid" alt="Preview Image">
+                        @endif
+                    </form>
+                </div>
+            </div>
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h3>{{ $name }}</h3>
                 </div>
             </div>
         </div>
